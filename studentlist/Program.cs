e8ef3b4f0b55a117615b7a63f2545295f47dd904
MoonHyekyung <moonsoft02@gmail.com>
2020-05-19 11:14:46 +0900

    list 생성 후 Add하기

--- studentlist/Program.cs
+++ studentlist/Program.cs
@@ -8,8 +8,24 @@ namespace studentlist
 {
     class Program
     {
+        class Teacher
+        {
+            public string name;
+            public string major;
+        }
         static void Main(string[] args)
         {
+            List<Teacher> list = new List<Teacher>();
+            list.Add(new Teacher() { name = "문혜경", major = "뉴미디어소프트웨어과" });
+            list.Add(new Teacher() { name = "이예진", major = "뉴미디어소프트웨어과" });
+            list.Add(new Teacher() { name = "곽가연", major = "뉴미디어웹솔루션과" });
+            list.Add(new Teacher() { name = "정유경", major = "뉴미디어웹솔루션과" });
+            list.Add(new Teacher() { name = "하나랑", major = "뉴미디어디자인과" });
+            list.Add(new Teacher() { name = "고가은", major = "뉴미디어디자인과" });
+
+            foreach(var item in list){
+                Console.WriteLine(item.name + " : " + item.major);
+            }
         }
     }
 }
